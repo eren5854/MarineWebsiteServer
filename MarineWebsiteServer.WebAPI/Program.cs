5762bc01bbf587a2c617ab05435cdf1489539d4d
eren5854 <ihsanerendelibas@hotmail.com>
2024-09-26 23:45:51 +0300

    .

--- MarineWebsiteServer.WebAPI/Program.cs
+++ MarineWebsiteServer.WebAPI/Program.cs
@@ -79,7 +79,7 @@ builder.Services.AddSwaggerGen(setup =>
         In = ParameterLocation.Header,
         Type = SecuritySchemeType.Http,
         Scheme = JwtBearerDefaults.AuthenticationScheme,
-        Description = "Put **_ONLY_** yourt JWT Bearer token on textbox below!",
+        Description = "Put **_ONLY_** your JWT Bearer token on textbox below!",
 
         Reference = new OpenApiReference
         {
